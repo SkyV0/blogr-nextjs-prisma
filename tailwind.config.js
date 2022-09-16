af09ed4d0cbfa13d0683cd6905bca97bd9e3ba5d
Skyler Vautier <98610597+SkyV0@users.noreply.github.com>
2022-09-16 15:45:10 -0400

    push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { fontFamily } = require('tailwindcss/defaultTheme');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
